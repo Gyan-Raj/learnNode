ca3f49fddc029e3fe62157463f6c2efdd327ddf3
dev2 <dev2@techpyramid.com>
2024-08-04 12:06:27 +0530

    solved the problem of forever sending the request by using res.end()

--- session1_1/index.js
+++ session1_1/index.js
@@ -1,13 +1,17 @@
-console.log("Server started");
-console.log("Server running");
-console.log("Server running");
+let PORT = 8082;
 
-const http = require("http"); //With Node.js installation we get a module named http, which is used to create the server(http.createServer((req,res)=>{})) using Node.js which can receive requests(req) and return a response(res)
+const http = require("http");
 
 const server = http.createServer((req, res) => {
-  console.log("Request received");
-}); //Use the createServer method on the http module to create a server. The createServer() method takes in a callback which is executed once the server receives a request from the client.
-let PORT = 8082;
+  const date = new Date().toLocaleDateString();
+  const time = new Date().toLocaleTimeString();
+  console.log(date, time);
+  res.end(); //The res object has a special function end() which we can call to mark the end of the response. This will make sure the request is completed.
+  //BUT, ending the response is just not enough, we also want to send data to the client.
+
+  //To send some information back to the client like a JSON or an HTML string. We use res.write() function which takes the data to be returned as an argument. It is in form of a string. We will do it in session1_2
+});
+
 server.listen(PORT, () => {
   console.log(`Server listening on port: ${PORT}`);
-}); //Finally we make this server instance “listen” for any requests. Specify the PORT number as the first argument (say 8082), and a success callback as the second argument
+});
