adddc124c9025f9bfbf9eba943649e2a0937e6e7
dev2 <dev2@techpyramid.com>
2024-08-04 11:41:19 +0530

    created .rest file where we created a GET request, where we never got any response because we never specified what response should we send back from server

--- session1/index.js
+++ session1/index.js
@@ -1,13 +1,13 @@
 console.log("Server started");
 console.log("Server running");
+console.log("Server running");
 
 const http = require("http"); //With Node.js installation we get a module named http, which is used to create the server(http.createServer((req,res)=>{})) using Node.js which can receive requests(req) and return a response(res)
 
 const server = http.createServer((req, res) => {
   console.log("Request received");
 }); //Use the createServer method on the http module to create a server. The createServer() method takes in a callback which is executed once the server receives a request from the client.
-let PORT=8082;
+let PORT = 8082;
 server.listen(PORT, () => {
-    console.log(`Server listening on port: ${PORT}`);
-    
+  console.log(`Server listening on port: ${PORT}`);
 }); //Finally we make this server instance “listen” for any requests. Specify the PORT number as the first argument (say 8082), and a success callback as the second argument
