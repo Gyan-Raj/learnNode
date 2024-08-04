7d7d955a02a6b1f87610b87a2ea18c8eb550a220
dev2 <dev2@techpyramid.com>
2024-08-04 14:55:33 +0530

    wrote a basic http request using write() and gave the content type using writeHead()

---
+++ session1_2/index.js
@@ -0,0 +1,23 @@
+let PORT = 8082;
+
+const http = require("http");
+
+const server = http.createServer((req, res) => {
+  // const date = new Date().toLocaleDateString();
+  // const time = new Date().toLocaleTimeString();
+  // console.log(date, time); //To send some information back to the client like a JSON or an HTML string we use res.write() function which takes the data to be returned as an argument.
+  // // res.write(date, time); //ERROR=> It should be in form of a string
+  // res.write(`${date}, ${time}`); //BUT now have a look at the response in Postman.  The type of the response is “TEXT” and not “JSON”. It also looks like plain string, not an object?
+  // //So, We must tell the client what “type of content” are we sending.
+
+  const date = new Date().toLocaleDateString();
+  const time = new Date().toLocaleTimeString();
+  res.writeHead(200, { "Content-Type": "application/json" });
+  res.write(`${date}, ${time}`); //To specify the type of content, we have a function writeHead() where we can specify Headers (like “Content-Type”) and the status code for the request
+
+  res.end();
+});
+
+server.listen(PORT, () => {
+  console.log(`Server listening on port: ${PORT}`);
+});
