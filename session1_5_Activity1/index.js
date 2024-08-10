0d8932b51f1ebc0f32e272200c77872a0d46571a
dev2 <dev2@techpyramid.com>
2024-08-10 18:17:12 +0530

    completed an activity

---
+++ session1_5_Activity1/index.js
@@ -0,0 +1,26 @@
+const http = require("http");
+const crypto = require("crypto");
+const currensiesJSON = require("./currencies.json");
+const PORT = 8082;
+const server = http.createServer((req, res) => {
+  let reqId = crypto.randomUUID();
+  switch (req.url) {
+    case "/":
+      res.writeHead(200, { "content-type": "text/html", "x-req-id": reqId });
+      res.write(`<h1>Currency Database</h1>`);
+      break;
+    case "/currencies":
+      res.writeHead(200, {
+        "content-type": "application/json",
+        "x-req-id": reqId,
+      });
+      res.write(JSON.stringify(currensiesJSON));
+      break;
+    default:
+      res.writeHead(404);
+  }
+  res.end();
+});
+server.listen(PORT, () => {
+  console.log(`Server listening on port: ${PORT}`);
+});
