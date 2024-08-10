b24c9c168ba0f53b46d7ae8153c21b4db906a766
dev2 <dev2@techpyramid.com>
2024-08-10 17:32:36 +0530

    learnt about req.url, using which we can send response conditionally

--- session1_4/index.js
+++ session1_4/index.js
@@ -4,19 +4,36 @@ const http = require("http");
 const crypto = require("crypto");
 
 const server = http.createServer((req, res) => {
-  const serverInfo = {
-    serverName: "Session1_4 server",
-    version: "1.0.0",
-    currentDate: new Date().toDateString(),
-    currentTime: new Date().toTimeString(),
-  };
-
   let reqId = crypto.randomUUID();
-  res.writeHead(200, { "Content-Type": "application/json", "x-req-id": reqId });
   console.log(reqId);
-
-  res.write(JSON.stringify(serverInfo));
-
+  if (req.url === "/") {
+    const serverInfo = {
+      serverName: "Session1_4 server",
+      version: "1.0.0",
+      currentDate: new Date().toDateString(),
+      currentTime: new Date().toTimeString(),
+    };
+    res.writeHead(200, {
+      "Content-Type": "application/json",
+      "x-req-id": reqId,
+    });
+    res.write(JSON.stringify(serverInfo));
+  } else if (req.url === "/users") {
+    res.writeHead(200, { "Content-Type": "text/html", "x-req-id": reqId });
+    res.write(`
+      <h1>Hello Users</h1>
+      <ul>
+        <li>User1</li>
+        <li>User2</li>
+        <li>User3</li>
+      </ul>
+      `);
+  } else {
+    res.writeHead(400, { "Content-Type": "text/html", "x-req-id": reqId });
+    res.write(`
+      <h1>Page not found</h1>
+      `);
+  }
   res.end();
 });
 
